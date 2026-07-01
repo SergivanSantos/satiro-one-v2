1320c6e87795a258dd5ce31f263f20dcf2f08709
SergivanSantos <sergivansantos@gmail.com>
2026-06-30 23:03:46 -0300

    Com fases4,funcionarios,dashboard, ainda sem Ordem de serviços

--- install-flutter.sh
+++ install-flutter.sh
@@ -3,4 +3,5 @@ curl -sLO https://storage.googleapis.com/flutter_infra_release/releases/stable/l
 tar -xf flutter_linux_3.29.0-stable.tar.xz
 export PATH="$PWD/flutter/bin:$PATH"
 git config --global --add safe.directory "*"
+echo "export PATH=\"$PWD/flutter/bin:\$PATH\"" >> $HOME/.bashrc
 flutter --version
