f788e5ea1f9b72816900c6df24e407d7eab547e2
SergivanSantos <sergivansantos@gmail.com>
2026-07-23 18:23:22 -0300

    Com fases,funcionarios,vibra&som10,ícones_orange

--- lib/features/servicos/models/servico.dart
+++ lib/features/servicos/models/servico.dart
@@ -26,7 +26,7 @@ class Servico {
   });
 
   factory Servico.fromMap(Map<String, dynamic> map) {
-    final popData = map['pops'] as Map<String, dynamic>?;   // ← Corrigido (plural)
+    final popData = map['pop'] as Map<String, dynamic>?;   // ← Corrigido (plural)
     final categoriaData = map['categoria'] as Map<String, dynamic>?;
 
     return Servico(
